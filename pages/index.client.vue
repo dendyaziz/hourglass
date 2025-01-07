1f1004d0362ab84b722d7bf96846f24aa783d1e4
Dendy <dendy.aziz@gmail.com>
2025-01-08 06:01:25 +0700

    init(prayer-time): fix container size

--- pages/index.client.vue
+++ pages/index.client.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
+import debounce from 'lodash.debounce'
 import { useConfetti } from '~/composables/useConfetti'
 import { getDiffToNextSecond, getTimeString } from '~/helper'
 
 const confetti = useConfetti()
-let timeElement: Element | null = null
+
+const fontSize: number = 100
+
+let containerElement: HTMLElement | null = null
+let timeElement: HTMLElement | null = null
 let timeInterval: NodeJS.Timeout | null = null
 let eventObserverInterval: NodeJS.Timeout | null = null
 
@@ -40,6 +45,20 @@ function startEventChecker() {
 // Set initial displayed time
 const initialTimeString = getCurrentTimeString()
 
+function syncFontSize() {
+  if (!containerElement)
+    return
+
+  const containerRect = containerElement.getBoundingClientRect()
+
+  const { height } = containerRect
+
+  containerElement.style.fontSize = `${height / 3.25}px`
+}
+
+let resizeObserver: ResizeObserver
+const debouncedSyncFontSize = debounce(syncFontSize, 5)
+
 onMounted(() => {
   timeElement = document.getElementById('time')
 
@@ -50,18 +69,36 @@ onMounted(() => {
 
   // Wait until the next second to start
   setTimeout(startEventChecker, diffToNextSecond)
+
+  containerElement = document.getElementById('clock-container')
+
+  if (containerElement) {
+    resizeObserver = new ResizeObserver(debouncedSyncFontSize)
+    resizeObserver.observe(containerElement)
+  }
+})
+
+onBeforeUnmount(() => {
+  // Disconnect the observer when the component is destroyed
+  if (resizeObserver) {
+    resizeObserver.disconnect()
+  }
 })
 </script>
 
 <template>
   <div class="h-dvh content-center">
-    <div class="relative aspect-[16/9] mx-auto max-h-dvh flex flex-col justify-center items-center h-full bg-base-content/10">
-      <div class="font-mono font-semibold text-[8vh]">
+    <div
+      id="clock-container"
+      class="relative aspect-[16/9] mx-auto max-h-dvh flex flex-col justify-center items-center bg-base-content/10 text-[132px]"
+      :style="{ fontSize: `${fontSize}px` }"
+    >
+      <div class="font-mono font-semibold text-[0.5em] truncate max-w-full">
         <PrayerCountDown />
       </div>
       <div
         id="time"
-        class="font-mono font-bold text-[28vh]"
+        class="font-mono font-bold text-[1em] truncate max-w-full"
       >
         {{ initialTimeString }}
       </div>
